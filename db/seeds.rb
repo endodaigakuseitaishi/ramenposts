e39562962beac74abee3988bad0e3285754aa961
遠藤大志 <taishi.e0616@yahoo.ne.jp>
2022-01-26 15:05:53 +0900

    faker導入

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-5.times do
-  shuta.posts.create!(
+10.times do
+  Post.create!(
     title: Faker::Lorem.sentence(word_count: 5),
     content: Faker::Lorem.sentence(word_count: 100)
   )
