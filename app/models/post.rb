d2f8b93cdc203e47f8036ecd9a15c87b0228c054
遠藤大志 <taishi.e0616@yahoo.ne.jp>
2022-01-26 20:54:42 +0900

    has_many, belongs_to

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,8 +8,15 @@
 #  title      :string
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  user_id    :integer
+#
+# Indexes
+#
+#  index_posts_on_user_id  (user_id)
 #
 class Post < ApplicationRecord
+  belongs_to :user
+
   validates :title, length: { minimum: 2 }
   validates :content, length: { maximum: 140 }
 
