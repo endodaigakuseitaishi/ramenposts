2b192c2fd02c7ee6e7391939a45e0580f878b749
遠藤大志 <taishi.e0616@yahoo.ne.jp>
2022-01-26 16:40:44 +0900

    before_action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,10 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new 
@@ -22,11 +22,9 @@ class PostsController < ApplicationController
   end
 
     def edit
-      @post = Post.find(params[:id])
     end
 
     def update
-      @post = Post.find(params[:id])
       if @post.update(post_params)
         redirect_to post_path(@post), notice: '更新完了'
       else
@@ -44,5 +42,9 @@ class PostsController < ApplicationController
   private 
   def post_params
     params.require(:post).permit(:title, :content)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
