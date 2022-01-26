dce566fe12f24cbcb580bd52976d295452a9b9c2
遠藤大志 <taishi.e0616@yahoo.ne.jp>
2022-01-26 23:42:59 +0900

    Profileモデルを作成

---
+++ app/models/profile.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: profiles
+#
+#  id           :integer          not null, primary key
+#  birthday     :date
+#  introduction :text
+#  nickname     :string
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#  user_id      :integer          not null
+#
+# Indexes
+#
+#  index_profiles_on_user_id  (user_id)
+#
+class Profile < ApplicationRecord
+  belongs_to :user
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_26_132307) do
+ActiveRecord::Schema.define(version: 2022_01_26_143917) do
 
   create_table "comments", force: :cascade do |t|
     t.integer "post_id", null: false
@@ -28,6 +28,16 @@ ActiveRecord::Schema.define(version: 2022_01_26_132307) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "nickname"
+    t.text "introduction"
+    t.date "birthday"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
