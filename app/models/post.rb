40b11edaa68662f0cd16eb16410af6910fd8b9b8
遠藤大志 <taishi.e0616@yahoo.ne.jp>
2022-01-26 19:57:37 +0900

    長さのvalidation

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,9 @@
 #  updated_at :datetime         not null
 #
 class Post < ApplicationRecord
+  validates :title, length: { minimum: 2 }
+  validates :content, length: { maximum: 140 }
+
   def display_created_at
     I18n.l(self.created_at, format: :default)
   end
