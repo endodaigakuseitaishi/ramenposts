06be37b56741eb230760fc4ebed3610dfb5c5bdc
遠藤大志 <taishi.e0616@yahoo.ne.jp>
2022-01-30 22:20:32 +0900

    可読性を意識したリファクタリング

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -22,16 +22,20 @@ require('bootstrap'); // Bootstrapを追加
 import $ from 'jquery'
 import axios from 'axios'
 
+const handleHeartDisplay = (hasLiked) => {
+  if (hasLiked) {
+    $('.active-heart').removeClass('hidden')
+  } else {
+    $('.inactive-heart').removeClass('hidden')
+  }
+} 
+
 document.addEventListener('turbolinks:load', () => {
   const dataset = $('#post-show').data()
   const postId = dataset.postId
   axios.get(`/posts/${postId}/like`)
     .then((response) => {
       const hasLiked = response.data.hasLiked
-      if (hasLiked) {
-        $('.active-heart').removeClass('hidden')
-      } else {
-        $('.inactive-heart').removeClass('hidden')
-      }
+      handleHeartDisplay(hasLiked)
     })
 })
